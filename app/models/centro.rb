05b63475ff9204ca0164aa037f1c0aa747a8bc8a
Jose Jerez <joseignaciojerez@gmail.com>
2024-03-03 19:22:52 -0300

    atencion form

--- app/models/centro.rb
+++ app/models/centro.rb
@@ -1,4 +1,4 @@
 class Centro < ApplicationRecord
-  has_many :atenciones, dependent: :destroy
+  has_many :atenciones, class_name: 'Atencion', dependent: :destroy
   validates :nombre, :area, :comuna, :modalidad, presence: true
 end
